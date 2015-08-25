96b787d80712a06a24ce33c3a08f1fc4dfe971de
Nicolas Schmidt <nfschmidt@gmail.com>
2015-08-25 14:29:20 -0300

    Added more tests on chapter 1 with the new testing mini framework

--- little-schemer/testing.scm
+++ little-schemer/testing.scm
@@ -1,13 +1,10 @@
-
-
+ 
 (define new-test
   (lambda (x y)
     (cons x (cons y '()))))
 
 
-(define get-description
-  (lambda (test)
-    (car test)))
+(define get-description car)
 
 
 (define get-result
@@ -44,16 +41,24 @@
 			       (display x out)))))
 
 
+(define l->string
+  (lambda (l)
+    (cond ((null? l) "")
+	  (else (string-append (->string (car l))
+			       " "
+			       (l->string (cdr l)))))))
+
+
 (define-syntax test
-  (syntax-rules (with-value with-values without-values is)
-    ((_ function (with-aruments args ...) is result)
-     (new-test (->string '(function with aruments args ... is result))
-	       (equal? result (function args ...))))
+  (syntax-rules (with-argument with-arguments without-arguments is)
+    ((_ function (with-arguments arg ...) is result)
+     (new-test (l->string '(function with arguments arg ... is result))
+	       (equal? result (function arg ...))))
 
     ((_ function (with-argument arg) is result)
-     (new-test (->string '(function with argument arg is result))
+     (new-test (l->string '(function with argument arg is result))
 	       (equal? result (function arg))))
 
     ((_ function (without-arguments) is result)
-     (new-test (->string '(function without arguments is result))
+     (new-test (l->string '(function without arguments is result))
 	       (equal? result (function))))))
--- little-schemer/tests-chapter01.scm
+++ little-schemer/tests-chapter01.scm
@@ -2,13 +2,46 @@
 (load "chapter01.scm")
 
 
-(print-tests-results (new-test "Harry is an atom"
-			       (atom? 'Harry))
+(print-tests-results
 
-		     (new-test "(Harry had a heap of apples) is not an atom"
-			       (not (atom? '(Harry had a heap of apples))))
+ (test atom? (with-argument 'atom) is #t)
 
-		     (new-test "() is not an atom"
-			       (not (atom? '())))
+ (test atom? (with-argument 'turkey) is #t)
+ 
+ (test atom? (with-argument 1492) is #t)
 
-		     )
+ (test atom? (with-argument 'u) is #t)
+
+ (test atom? (with-argument '*abc$) is #t)
+
+ (test list? (with-argument '(atom)) is #t)
+
+ (test list? (with-argument '(atom turkey or)) is #t)
+ 
+ (test list? (with-argument '((atom turkey) or)) is #t)
+
+ (test list? (with-argument '(how are you doing so far)) is #t)
+
+ (test list? (with-argument '(((how) are) ((you) (doing so)) far)) is #t)
+
+ (test list? (with-argument '()) is  #t)
+
+ (test atom? (with-argument '()) is  #f)
+
+ (test list? (with-argument '(() () () ())) is #t)
+
+ (test car (with-argument '(a b c)) is 'a)
+
+ (test car (with-argument '((a b c) x y z)) is '(a b c))
+
+ (test car (with-argument '(((hotdogs)) (and) (pickle) relish)) is '((hotdogs)))
+
+ (test cdr (with-argument '(a b c)) is '(b c))
+
+ (test cdr (with-argument '((a b c) x y z)) is '(x y z))
+
+ (test cdr (with-argument '(hamburger)) is '())
+
+ (test cdr (with-argument '((x) t r)) is '(t r))
+ 
+ )
