aa9fa712c56624b774d42e30f8177b82be2a5901
Nicolas Schmidt <nfschmidt@gmail.com>
2015-08-25 12:21:01 -0300

    Refactoring in testing.scm, also added a macro for future use

--- little-schemer/testing.scm
+++ little-schemer/testing.scm
@@ -1,12 +1,8 @@
 
-(define new-pair
+
+(define new-test
   (lambda (x y)
     (cons x (cons y '()))))
-
- 
-(define new-test
-  (lambda (description result)
-    (new-pair description result)))
 
 
 (define get-description
@@ -19,19 +15,22 @@
     (car (cdr test))))
 
 
-(define test-passed
+(define test-passed get-result)
+
+
+(define test->string
   (lambda (test)
-    (get-result test)))
+    (string-append (get-description test)
+		   " ................... "
+		   (if (test-passed test)
+		       " OK"
+		       " FAILED")
+		   "\n")))
 
 
 (define print-test-result
   (lambda (test)
-    (display (string-append (get-description test)
-			    " ................... "
-			    (if (test-passed test)
-				" OK"
-				" FAILED")
-			    "\n"))))
+    (display (test->string test))))
 
 
 (define print-tests-results
@@ -39,3 +38,22 @@
     (for-each print-test-result tests)))
 
 
+(define ->string
+  (lambda (x)
+    (call-with-output-string (lambda (out)
+			       (display x out)))))
+
+
+(define-syntax test
+  (syntax-rules (with-value with-values without-values is)
+    ((_ function (with-aruments args ...) is result)
+     (new-test (->string '(function with aruments args ... is result))
+	       (equal? result (function args ...))))
+
+    ((_ function (with-argument arg) is result)
+     (new-test (->string '(function with argument arg is result))
+	       (equal? result (function arg))))
+
+    ((_ function (without-arguments) is result)
+     (new-test (->string '(function without arguments is result))
+	       (equal? result (function))))))
